e25771eff3a54ca25bec8985606b108e8af67a6a
一只贾 <3378079760@qq.com>
2023-07-23 15:50:46 +0000

    比特的视频学习
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 99乘法表.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int j,i;
+	printf("\t\t\t¾Å¾Å³Ë·¨±í\n");
+	for(j=1;j<=9;j++){
+	   for(i=1;i<=j;i++)	 
+
+	  	  printf("%d*%d=%d\t",j,i,j*i);
+	  	  printf("\n");
+}
+	  return 0;
+}
