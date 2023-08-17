ed625ba713f3a2102d04f90c6c92acc689025e5a
一只贾 <3378079760@qq.com>
2023-08-17 12:13:21 +0000

    计算机储存
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ test.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<windows.h>
+int main() {
+	unsigned char i;
+	for (i = 0; i <= 255; i++) {
+		printf("hello world\n");
+		Sleep(100);//ÑÓ³Ù´òÓ¡ËÙ¶È
+	}
+	
+	return 0;
+}
