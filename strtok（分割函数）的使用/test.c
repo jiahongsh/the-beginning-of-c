82e2605490187a8eb40a4673dc47343394130bf5
一只贾 <3378079760@qq.com>
2023-09-29 03:53:56 +0000

    strtok
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ strtok（分割函数）的使用/test.c
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+	char arr1[] = "jiahongsheng@3378079760.com";
+	char p[] = "@";
+	char* ret=strtok(arr1, p);
+	printf("%s\n", ret);
+	ret = strtok(NULL, p);
+	printf("%s\n", ret);
+	ret = strtok(NULL, p);
+	printf("%s\n", ret);
+	return 0;
+}
