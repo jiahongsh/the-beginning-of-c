af6c7a6570fbba4faf7699873f37ade9e36f9313
一只贾 <3378079760@qq.com>
2023-09-29 10:02:27 +0000

    fopen
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ fopen的使用（浅）/test.c
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<errno.h>
+int main() {
+	FILE* pf = fopen("test.test", "r");
+	if (pf ==NULL) {
+		printf("%s\n", strerror(errno));
+	}
+	else {
+		;
+	}
+	return 0;
+}
