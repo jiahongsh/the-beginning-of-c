b5e2d3ef3b73f2f7afe71555560e44190adf5593
一只贾 <3378079760@qq.com>
2023-09-30 17:41:30 +0000

    for语句的运用
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 三角空格图案/test.c
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+	int n = 0;
+	printf("请输入三角形规格：");
+	scanf("%d", &n);
+	int i = 0;
+	int arr[100][100] = { 0 };
+
+	for (i = 0; i < n; i++) {//多行
+		//一行
+		int j = 0;
+		for (j = 0; j < n - 1 - i; j++) {
+			printf("  ");
+		}
+		for (j = n - 1 - i; j < n; j++) {
+			printf("* ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
