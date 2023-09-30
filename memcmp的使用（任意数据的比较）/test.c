87c50affcb5fa20d50c8ebe8c801122e691ef44f
一只贾 <3378079760@qq.com>
2023-09-30 07:17:37 +0000

    memcpy
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ memcmp的使用（任意数据的比较）/test.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+	int arr1[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int arr2[] = { 1,2,3};
+	int ret = memcmp(arr1, arr2, 12);
+	if (ret < 0) {
+		printf("arr1<arr2\n");
+	}
+	else if(ret==0){
+		printf("arr1==arr2\n");
+	}
+	else {
+		printf("arr1>arr2\n");
+	}
+	return 0;
+}
