7aaf6b4beee165dea7dd6200c8276a66273e53ed
一只贾 <3378079760@qq.com>
2023-10-01 19:50:03 +0000

    大端与小端
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 数据的储存 （习题）/test.c
@@ -0,0 +1,9 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+	unsigned int a = 0x1234;
+	unsigned char b = *(unsigned char*)&a;
+	printf("%x\n", b);///小端34，大端00
+	return 0;
+}
