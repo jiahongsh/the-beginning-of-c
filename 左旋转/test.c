6618d22e4a1c90cfc82c5d7cb302b15beb4a86a3
一只贾 <3378079760@qq.com>
2023-10-02 19:31:19 +0000

    for语句的运用
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 左旋转/test.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+//输入一个字符串，使所有字符串左移动一个
+void left_move(char* arr1, int ret) {
+	char temp = arr1[0];
+	int i = 0;
+	for (i = 0; i < ret - 1; i++) {
+		arr1[i] = arr1[i + 1];
+	}
+	arr1[i] = temp;
+}
+
+int main() {
+	char arr1[] = "abcedf";
+	int k = 0;
+	scanf("%d", &k);
+	int i = 0;
+	int ret = strlen(arr1);
+	for (i = 0; i < k; i++) {
+	
+		left_move(arr1, ret);
+		
+	}
+	printf("%s\n", arr1);
+	return 0;
+}
