a500c0c72c6a278adb2eb7b16c059e6a6baf3832
一只贾 <3378079760@qq.com>
2023-10-03 15:04:15 +0000

    for语句的运用和规律的观察
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 上三角矩阵判定/test.c
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+	int arr[10][10] = { 0 };
+	int i = 0;
+	int j = 0;
+	int n = 0;
+	int flag = 1;
+	scanf("%d", &n);
+	//输入
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			scanf("%d", &arr[i][j]);
+		}
+	
+	}
+	//输出
+	for (i = 0; i < n; i++) {
+		/*for (j = 0; j < i; j++) {
+			if (arr[i][i] != 0) {
+				flag == 0; 
+				}*/
+		for (j = 0; j < n; j++) {
+			if (i > j) {
+				if (arr[i][j] != 0) {
+					flag = 0;
+				}
+			}
+		  }
+		}
+
+	//判定
+	if (flag ==1) {
+		printf("yes\n");
+	}
+	else if (flag == 0) {
+		printf("no\n");
+	}
+	return 0;
+}
