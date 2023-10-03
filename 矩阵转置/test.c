5573978e95f63efd572643aaa5f53f4ad9acab19
一只贾 <3378079760@qq.com>
2023-10-03 04:00:44 +0000

    数学
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 矩阵转置/test.c
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//输入一个数组，将其旋转输出
+//1 2 3  1 4
+//4 5 6  2 5
+//       3 6
+
+int main() {
+	int arr[10][10] = {0};
+	int n = 0;
+	int m = 0;
+	scanf("%d %d", &n, &m);
+	//输入
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < m; j++) {
+			scanf("%d", &arr[i][j]);
+		}
+	}
+	//输出
+	for (i = 0; i < m; i++) {
+		for (j = 0; j < n; j++) {
+			printf("%d ", arr[j][i]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	return 0;
+}
