e9df4eacc5e2288af78d64dab79bdf860afc9842
一只贾 <3378079760@qq.com>
2023-10-04 16:48:20 +0000

    初识位段
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 位段的使用和内存对齐/test.c
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+	//冒号后是分配几个bit位
+	struct s {
+		int a : 2;
+		int b : 5;
+		int c : 10;
+		int d : 30;
+	};
+	//共37个bit位，六个字节可放下
+	printf("%d", sizeof(struct s));//内存对齐故需要8个bit位
+	return 0;
+}
