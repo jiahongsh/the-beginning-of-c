c671b49a26fe7c8d8c93c1da2c83599681bfb527
一只贾 <3378079760@qq.com>
2023-10-04 10:03:44 +0000

    结构体
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 结构体传参/tset.c
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+struct str {
+		int data[100];
+		int num;
+	};
+//结构体接收
+void printf1(struct str ss) {
+	int i = 0;
+	for (i = 0; i < 3; i++) {
+		printf("%d ", ss.data[i]);
+	}
+	printf("%d\n", ss.num);
+}
+//结构体指针接收
+void printf2(struct str* ps) {
+	int i = 0;
+	for (i = 0; i < 3; i++) {
+		printf("%d ", ps->data[i]);
+	}
+	printf("%d\n",ps->num);
+}
+
+
+int main() {
+	struct str s = { {1,2,3},100 };
+	printf1(s);
+	printf2(&s);
+	return 0;
+}
