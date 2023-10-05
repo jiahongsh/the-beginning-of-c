065cda35886e8ddb4385aeca342877a89c145390
一只贾 <3378079760@qq.com>
2023-10-05 19:02:00 +0000

    联合的使用
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 联合的内存对齐/test.c
@@ -0,0 +1,13 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+	union un {
+		char arr[5];//对齐数为1
+		int i;//对齐数为4
+	};
+	//根据内存对齐原则，所占字节数为4的倍数
+	printf("%d\n", sizeof(union un));
+
+	return 0;
+}
