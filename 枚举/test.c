6fb6e20ebda923d66bc71342abd88205ace8530a
一只贾 <3378079760@qq.com>
2023-10-05 18:03:24 +0000

    枚举
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 枚举/test.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+	enum day {//日期
+		mon,
+		tues,
+		wed,
+		thur,
+		fri,
+		sat,
+		sun,
+		
+
+	};
+	printf("%d\n", mon);
+	printf("%d\n", tues);
+	printf("%d\n", wed);
+	printf("%d\n", thur);
+	printf("%d\n", fri);
+	printf("%d\n", sat);
+	printf("%d\n", sun);
+	return 0;
+}
