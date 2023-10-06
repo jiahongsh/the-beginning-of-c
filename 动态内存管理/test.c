2d1b1c7bd934a7bcea750803e206f814c9e612cb
一只贾 <3378079760@qq.com>
2023-10-06 08:51:06 +0000

    update 动态内存管理/test.c.
    Signed-off-by: 一只贾 <3378079760@qq.com>

--- 动态内存管理/test.c
+++ 动态内存管理/test.c
@@ -1,4 +1,4 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -7,6 +7,9 @@ int main() {
 	int arr[5] = { 0 };
 	int* p = (int*)malloc(20);//动态内存的开辟
 	//赋值
+    if(p==NULL){//若开辟失败，则返回空指针；
+        printf("%s",strerror)
+    }
 	int i = 0;
 	for (i = 0; i < 4; i++) {
 		*(p + i) = i;
@@ -15,5 +18,7 @@ int main() {
 	for (i = 0; i < 4; i++) {
 		printf("%d ", *(p + i));
 	}
+
+    free(p);
 	return 0;
 }
