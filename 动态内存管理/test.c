d73460ddd034f7be600188f55d65ba3aa4123d98
一只贾 <3378079760@qq.com>
2023-10-06 05:03:08 +0000

    动态内存的管理
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 动态内存管理/test.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main() {
+	int arr[5] = { 0 };
+	int* p = (int*)malloc(20);//动态内存的开辟
+	//赋值
+	int i = 0;
+	for (i = 0; i < 4; i++) {
+		*(p + i) = i;
+	}
+	//输出
+	for (i = 0; i < 4; i++) {
+		printf("%d ", *(p + i));
+	}
+	return 0;
+}
