943f6a98967b2d30d3d7ad5be7e2fb7dc882b713
一只贾 <3378079760@qq.com>
2023-10-06 09:36:51 +0000

    calloc函数的使用
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ calloc函数的使用/test.c
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<string.h>
+int main() {
+	int arr[5] = { 0 };
+	int* p = (int*)calloc(10,sizeof(arr[0]));//该方式声情内存会初始化
+	if (p == NULL) {
+		printf("%s\n", strerror(errno));
+	}
+	int i = 0;
+	for (i = 0; i < 10; i++) {
+		printf("%d ", *(p + i));
+	}
+	return 0;
+}
