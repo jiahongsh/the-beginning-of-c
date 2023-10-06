bbe286bcacb2797574733f37c3526e8a6c1d2988
一只贾 <3378079760@qq.com>
2023-10-06 10:25:04 +0000

    realloc
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ realloc函数的使用/test.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<errno.h>
+int main() {
+	int* p = (int*)malloc(40);
+	int i = 0;
+	for (i = 0; i < 10; i++) {
+		*(p + i) = i;
+	}
+	if (p == NULL) {
+		printf("%s\n", strerror(errno));
+	}
+	//在后追加40个字节的空间
+	int* ptr = realloc(p, 40);
+	if (ptr != NULL) {
+		p = ptr;
+	}
+
+	//输出
+	for (i = 0; i < 10; i++) {
+		printf("%d ", *(p + i));
+	}
+	return 0;
+}
