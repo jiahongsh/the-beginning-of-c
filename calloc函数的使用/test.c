fd7f750973cf6bca9e435cac42a267fb8fbba283
一只贾 <3378079760@qq.com>
2023-10-06 09:38:14 +0000

    update calloc函数的使用/test.c.
    Signed-off-by: 一只贾 <3378079760@qq.com>

--- calloc函数的使用/test.c
+++ calloc函数的使用/test.c
@@ -1,11 +1,11 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
 #include<errno.h>
 #include<string.h>
 int main() {
 	int arr[5] = { 0 };
-	int* p = (int*)calloc(10,sizeof(arr[0]));//该方式声情内存会初始化
+	int* p = (int*)calloc(10,sizeof(arr[0]));//该方式声情内存会初始化所有数据为0
 	if (p == NULL) {
 		printf("%s\n", strerror(errno));
 	}
