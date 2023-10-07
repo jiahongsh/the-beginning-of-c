01282994966d17f5fcc98bdf1a4ce969c3fa50ea
一只贾 <3378079760@qq.com>
2023-10-07 16:32:55 +0000

    字符串和野指针
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 字符串（习题）/test.c
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+char* con() {
+	char p[] = "hello world";//p被销毁，str是野指针
+	return p;
+}
+void test() {
+	char* str = NULL;
+	str = con();
+	printf(str);
+int main() {
+	test();
+	return 0;
+}
