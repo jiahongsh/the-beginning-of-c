e907be7baf9488f2492b397730e3c1bdd97ef75a
一只贾 <3378079760@qq.com>
2023-10-07 19:53:57 +0000

    内存的释放
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 野指针2（习题）/test.c
@@ -0,0 +1,16 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void test() {
+	char* str = (char*)malloc(100);
+	strcpy(str, "hello");
+	free(str);//str已释放，地址指向还存在，但内存不存在，str为野指针
+	if (str != NULL) {
+		strcpy(str, "world");
+		printf(str);
+	}
+}
+int main() {
+	test();
+	return 0;
+}
