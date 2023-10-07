e463ae7b7661833b68139affee311acf0a35c8db
一只贾 <3378079760@qq.com>
2023-10-07 16:56:14 +0000

    正确传参修改函数外的值
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 函数的传参与使用（习题）/test.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+//正确的传参修改str
+void con(char** p) {
+	*p = (char*)malloc(400);
+}
+
+	void test() {
+	char* str = NULL;
+	con(&str);
+	strcpy(str, "hello");
+	printf(str);
+	}
+
+	
+
+int main() {
+	test();
+	return 0;
+}
