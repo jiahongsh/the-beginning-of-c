32fa0184582832d5460dbd5bdee9b80782e1c6a2
一只贾 <3378079760@qq.com>
2023-10-07 12:06:15 +0000

    free
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 动态内存开辟忘记释放/test.c
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+void test() {
+	int* p = (int*)malloc(40);
+	int flag = 0;
+	scanf("%d", &flag);
+	if (flag == 5) {
+		return ;//此处直接return，不经过free的过程
+	}
+	free(p);
+	p = NULL;
+
+}
+int main() {
+	
+	test();
+	return 0;
+}
