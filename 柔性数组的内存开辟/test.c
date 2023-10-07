6a1ce9b2dead421aaef32851523ecc90c3f17c74
一只贾 <3378079760@qq.com>
2023-10-07 21:20:12 +0000

    柔性 数组
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 柔性数组的内存开辟/test.c
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<errno.h>
+
+struct s {
+	int i;
+	int arr[];
+};
+int main() {
+	struct s* p = (struct s*)malloc(sizeof(struct s) + 40);//含有柔性数组的结构体只能开辟动态内存
+	if (p == NULL) {
+		printf("%s\n", strerror(errno));
+		return;
+	}
+	int i = 0;
+	//输入
+	for (i = 0; i < 10; i++) {
+		p->arr[i] = i;
+	}
+	//输出
+	for (i = 0; i < 10; i++) {
+		printf("%d", p->arr[i]);
+	}
+	struct s* str = (struct s*)realloc(p, sizeof(struct s) + 80);//该数组大小可随时通过realloc函数改变
+	if (str != NULL) {
+		p = str;
+	}
+	return 0;
+}
