d8963e630eaa397a85f446d003e4a4e05837ed6c
一只贾 <3378079760@qq.com>
2023-10-07 12:33:49 +0000

    free
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 动态内存开辟忘记释放2/test.c
@@ -0,0 +1,13 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+int* tset() {
+	int* p = (int*)malloc(40);
+	if (p == NULL) {
+		return p;
+	}
+	return p;
+}
+int main() {
+	int* ret = tset();//由p开辟，由ret接收，忘记释放
+	return 0;
+}
