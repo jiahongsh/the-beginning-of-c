587f558ee892360e2df5f9d9656022e219eb5c77
一只贾 <3378079760@qq.com>
2023-10-07 09:55:18 +0000

    动态内存
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 动态内存使用的常见错误/test.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<string.h>
+int main() {
+	int*p=(int*)malloc(40);
+	/*if (p == NULL) {
+		printf("%s\n", strerror(errno));  
+		return 1;
+	}*/
+	*p = 10;//p可能是空指针需要判断一下
+	free(p);
+	p = NULL;
+		return 0; 
+}
