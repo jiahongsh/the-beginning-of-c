689d4bc9393407ee05a8be180278ce42e130f684
一只贾 <3378079760@qq.com>
2023-10-07 11:36:35 +0000

    free的错误使用
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ free释放一块动态内存的一部分/test.c
@@ -0,0 +1,14 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+	int* p = (int*)malloc(40);
+	int i = 0;
+	for (i = 0; i < 10; i++) {
+		*p = i;
+		p++;
+	}
+	free(p);//p的位置变了不能free原来p所指向开辟的空间
+	p = NULL;
+	return 0;
+}
