225c7334189a8966342de7ceacc65b7c03a4b9e6
一只贾 <3378079760@qq.com>
2023-10-08 08:41:25 +0000

    文件的应用
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 文件的读写/test.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<errno.h>
+
+int main() {
+	//FILE* pf = fopen("test.txt", "w"); 写文件
+	FILE* pf = fopen("test.txt", "r");//读文件
+	/*if ( pf== NULL) {
+		printf("%s\n", strerror(errno));
+		return 1;
+	}*/
+	//写文件
+	/*int i = 0;
+	for (i = 'a'; i < 'z'; i++) {
+
+		fputc(i, pf);
+	}*/
+	//读文件
+	int ch = 0;
+	while ((ch=fgetc(pf))!=EOF) {
+		printf("%c ", ch);
+	}
+	//关闭文件
+	fclose(pf);
+	pf = NULL;
+	return 0;
+}
