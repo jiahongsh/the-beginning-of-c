66cd9aacfccf4c7fb999e55765fd509e08c1ab8e
一只贾 <3378079760@qq.com>
2023-10-08 03:04:05 +0000

    文件
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 文件的打开与关闭/test.c
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<errno.h>
+
+int main() {
+	FILE*p=fopen("textt.txt", "r");
+	if (p == NULL) {
+
+		printf("%s", strerror(errno));
+		return;
+	}
+	//读文件
+    //。。。。/
+	//。。。/
+
+	fclose(p);
+	p = NULL;
+	//关闭文件
+	return 0;
+}
