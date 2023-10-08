72a7af594a427e513daf1d525c4e0ac47147a4f4
一只贾 <3378079760@qq.com>
2023-10-08 11:25:36 +0000

    perror函数的使用
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ perror函数的使用/test.c
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<errno.h>
+
+int main() {
+	FILE* pf = fopen("test.txt", "r"); //写文件
+	if (pf == NULL) {
+		perror("fopen");
+	}
+
+	//关闭文件
+	fclose(pf);
+//	pf = NULL;
+	return 0;
+}
