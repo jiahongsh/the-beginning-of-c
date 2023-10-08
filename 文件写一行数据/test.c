dec080b65e33e5ba220910f9bf50f33bc4cd0d0b
一只贾 <3378079760@qq.com>
2023-10-08 10:32:14 +0000

    fgets函数的应用
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 文件写一行数据/test.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<errno.h>
+
+int main() {
+	FILE* pf = fopen("test.txt", "w"); //写文件
+	//读一行数据
+	fputs("hello world", pf);
+
+	//关闭文件
+	fclose(pf);
+	pf = NULL;
+	return 0;
+}
