2a2bf70532df2f2c6f328cd8cdb81363fd6f1322
一只贾 <3378079760@qq.com>
2023-10-08 10:33:29 +0000

    fputs函数的使用
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 文件读一行数据/test.c
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<errno.h>
+
+int main() {
+	FILE* pf = fopen("test.txt", "r"); //写文件
+	//读一行数据
+	char arr[20];
+	gets(arr, 5, pf);
+
+	//关闭文件
+	fclose(pf);
+	pf = NULL;
+	return 0;
+}
