3ce24f261859e501ed8921bf128426a9d310b680
一只贾 <3378079760@qq.com>
2023-10-09 17:08:41 +0000

    文件的应用
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 通讯录（文件形式）/contact.h
@@ -0,0 +1,42 @@
+#pragma once
+#define maxname 20
+#define maxsex 10
+#define maxtele 12
+#define maxadder 30
+//打印菜单
+void menu();
+//通讯录的每个人
+typedef struct people {
+	char name[maxname];
+	int age;
+	char sex[maxsex];
+	char tele[maxtele];
+	char adder[maxadder];
+}people;
+//通讯录
+typedef struct contact {
+	people* data;//通讯录中的信息
+	int count;//通讯录中储存的人数
+	int cap;//当前通讯录的容量
+}contact;
+
+//初始化通讯录
+void initcontact(contact* con);
+//添加通讯录
+void addconstact(contact* con);
+//显示通讯录名单；
+void showcontact(contact* con);
+//删除通讯录
+void delcontact(contact* con);
+//查找通讯录信息
+void searchcontact(contact* con);
+//修改通讯录内信息
+void modifycontact(contact* con);
+//排列信息
+void sortconstact(contact* con);
+//销毁储存空间
+void distorycontact(contact* con);
+//储存信息到文件夹
+void savecontact(contact*con);
+//加载文件中的信息到通讯录
+void loadcontact(contact*con);
---
+++ 通讯录（文件形式）/test.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include"contact.h"
+
+
+int main() {
+	int input = 0;
+	contact con;//通讯录
+	//初始化通讯录
+	initcontact(&con);
+	do {
+		//打印菜单
+		menu();
+		printf("请选择功能:");
+		scanf("%d", &input);
+		//选项的执行
+		switch (input) {
+		case 1:
+			addconstact(&con);
+			break;
+		case 2:
+			delcontact(&con);
+			break;
+		case 3:
+			searchcontact(&con);
+			break;
+		case 4:
+			modifycontact(&con);
+			break;
+		case 5:
+			showcontact(&con);
+			break;
+		case 6:
+			sortconstact(&con);
+			break;
+		case 0:
+			savecontact(&con);
+			distorycontact(&con);
+			printf("退出通讯录\n");
+			break;
+		default:
+			printf("输入错误\n");
+			break;
+		}
+	} while (input);
+}
