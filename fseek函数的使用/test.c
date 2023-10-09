6c60bf4c11fc9994d1075083252965c66e4ca94f
一只贾 <3378079760@qq.com>
2023-10-09 17:27:09 +0000

    fseek函数
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ fseek函数的使用/test.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<errno.h>
+
+int main() {
+	//FILE* pf = fopen("test.txt", "w"); //写文件
+	FILE* pf = fopen("test.txt", "r");//读文件
+	//写文件
+	/*char i = 0;
+	for (i = 'a'; i < 'z'; i++) {
+		fputc(i, pf);
+	}*/
+	//读文件
+
+	fseek(pf, 2, SEEK_SET);
+int  ch = fgetc(pf);
+printf("%c ", ch);
+fseek(pf, -2, SEEK_END);
+  ch = fgetc(pf);
+printf("%c ", ch);
+fseek(pf, 1, SEEK_SET);
+ch = fgetc(pf);
+printf("%c ", ch);
+	
+
+
+
+	//关闭文件
+	fclose(pf);
+	pf = NULL;
+	return 0;
+}
