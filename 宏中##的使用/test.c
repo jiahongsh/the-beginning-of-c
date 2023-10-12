32e493ed52a2e99b3be84ab854fc10597b4d61d9
一只贾 <3378079760@qq.com>
2023-10-12 17:48:12 +0000

    ##
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 宏中##的使用/test.c
@@ -0,0 +1,8 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#define PRINTF(N,X) num##106//##能使两个字符串合并成为一个字符串
+int main() {
+	int num106 = 100;
+	printf("%d", PRINTF(num,106));
+	return 0;
+}
