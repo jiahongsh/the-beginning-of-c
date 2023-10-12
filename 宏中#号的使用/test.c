ee837080f1536cad5aeba42efac6af045aac69e9
一只贾 <3378079760@qq.com>
2023-10-12 17:47:49 +0000

    #
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 宏中#号的使用/test.c
@@ -0,0 +1,8 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#define PRINT(N) printf("the value "#N" is %d\n",N)//当在N前加一个#时N会替换成N所代表的字符串，及（a）
+int main() {
+	int a = 10;
+	PRINT(a);
+	return 0;
+}
