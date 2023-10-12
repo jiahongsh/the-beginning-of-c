7ec8a43dac174527effb4020db28061678884c34
一只贾 <3378079760@qq.com>
2023-10-12 01:49:43 +0000

    程序已经设定的返回符号
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 程序的编译时间和日期的打印，失败和返回的return/test.c
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+	
+	int i = 0;
+	FILE* pf = fopen("text.txt", "w");
+	if (pf == NULL) {
+		perror("fopen");
+		return EXIT_FAILURE;//失败返回
+		//return EXIT_SUCCESS;//成功返回
+	}
+	for (i = 0; i < 10; i++) {
+		printf("dile=%s line=%d date=%s time=%s i=%d\n", __FILE__, __LINE__, __DATE__, __TIME__, i);
+	}
+	return 0;
+}
