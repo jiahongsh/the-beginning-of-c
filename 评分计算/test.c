dc1483f7e82c6e378a291b6519a7afdaec6c1fad
一只贾 <3378079760@qq.com>
2023-10-13 06:14:46 +0000

    while
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 评分计算/test.c
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//评委现场打分，共七人，去除最大值和最小值，取平均
+int main() {
+	int n = 0;
+	int i = 0;
+	int sum = 0;
+	int max = 0;
+	int min = 0;
+	while (scanf("%d", &n)==1) {
+		
+		if (max < n) {
+			max = n;
+		}
+		else if (min > n) {
+			min = n;
+		}
+		sum += n;
+		i++;
+		if (i == 7) {
+			break;
+		}
+		
+	}
+	int pr = (sum - max - min)/5;
+	printf("%d \n", pr);
+	return 0;
+}
