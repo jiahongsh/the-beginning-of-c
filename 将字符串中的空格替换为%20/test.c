6ba62c4a71a790cb063dc5365fdb41d4a5bfdaa0
一只贾 <3378079760@qq.com>
2023-10-15 09:57:11 +0000

    字符串
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 将字符串中的空格替换为%20/test.c
@@ -0,0 +1,46 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+	char a[100] = "we are happy";
+	char* str = a;
+	int count = 0;
+	int n = 0;
+	//有几个空格
+	while (*str) {
+		if (*str == ' ') {
+			count++;
+		}
+		str++;
+
+	}
+	
+	char* end2 = (str+ count * 2-1);
+	char* end1 = str-1;
+	while (1) {
+		if (*end1 == ' ') {
+			*end2= 0;
+			end2--;
+			*end2= 2;
+			end2--;
+			*end2= '%';
+			end2--;
+			end1--;
+		}
+		*end2 = *end1;
+		end1--;
+		end2--;
+		if (end1 == a) {
+			break;
+		}
+		printf("%s\n", a);
+		
+	}
+	return 0;
+}
+
+
+int main() {
+
+	return 0;
+}
