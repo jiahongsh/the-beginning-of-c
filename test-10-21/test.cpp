8e0e20bad29ba0a61d1be49f1dcc66a9dedffef4
一只贾 <3378079760@qq.com>
2023-10-28 12:50:20 +0000

    hello world
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ test-10-21/test.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<easyx.h>
+int main() {
+	printf("hello world");
+	return 0;
+}
+
+//int main() {
+//	char c= 111;
+//	printf("%d", c);
+//	return 0;
+//}
+//
+////
+
+int main() {
+	int a = 0;
+	int b = 0;
+	scanf("%d %d", &a, &b);
+	printf("%d", a + b);
+}
