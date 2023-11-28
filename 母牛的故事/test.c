ae4d6df5a769933b650c6f5c95d8e72d9f122e60
一只贾 <3378079760@qq.com>
2023-11-28 15:49:33 +0000

    递归找规律
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 母牛的故事/test.c
@@ -0,0 +1,21 @@
+﻿//有一头母牛，它每年年初生一头小母牛。
+//每头小母牛从第四个年头开始，每年年初也生一头小母牛。
+//请编程实现在第n年的时候，共有多少头母牛？
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int fib(int n) {
+	if (n <= 3) {
+		return n;
+	}
+	return fib(n - 1) + fib(n - 3);
+}
+int main() {
+	int n = 0;
+	scanf("%d", &n);
+	int sum = fib(n);
+	
+	printf("%d", sum);
+	return 0;
+}
